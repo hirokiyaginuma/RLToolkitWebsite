8c732abe334c2ad4d4622853506b14abe032325b
Hiroki Yaginuma <hirokiyaginuma21@gmail.com>
2020-04-10 06:43:39 +0000

    First commit

---
+++ mysite/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os, sys, site
+
+from django.core.wsgi import get_wsgi_application
+
+site.addsitedir("/home/webapp/envs/webapp/lib/python3.6/site-packages")
+sys.path.append("/home/webapp/envs//")
+sys.path.append("/home/webapp/envs/mysite")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = get_wsgi_application()
